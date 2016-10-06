4d84ddb5408ddfc7e6cc6ec02967b31998e74909
Timot Tarjani <ttimotster@gmail.com>
2016-10-06 11:32:56 +0200

    Feature cards extended

--- app/controller/StartController.js
+++ app/controller/StartController.js
@@ -4,10 +4,27 @@
             $scope.headtext = 'StartAngular';
 
             $scope.cards = [
-                              'storage/images/kep1.jpg',
-                              'storage/images/kep2.jpg',
-                              'http://placehold.it/800x500',
-                              'http://placehold.it/800x501'
+                              {
+                                 'title' : 'Feature Label 1',
+                                 'image' : 'storage/images/kep1.jpg',
+                                 'description' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+                              },
+                              {                                 
+                                 'title' : 'Feature Label 2',
+                                 'image' : 'storage/images/kep2.jpg',
+                                 'description' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+                              },
+                              {                                 
+                                 'title' : 'Feature Label 3',
+                                 'image' : 'http://placehold.it/800x500',
+                                 'description' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+                              },
+                              {                                 
+                                 'title' : 'Feature Label 4',
+                                 'image' : 'http://placehold.it/800x500',
+                                 'description' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+                              },
+  
                               ];
 
 
